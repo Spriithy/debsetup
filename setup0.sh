f73c942d7991cfaa9593141ac8a4abf0bff67ee2
Théophile Dano <theo.dano@gmail.com>
2019-10-23 10:53:59 +0200

    Update setup0.sh

--- setup0.sh
+++ setup0.sh
@@ -2,6 +2,5 @@
 
 echo "export PATH=\$PATH:/usr/sbin/" >> ~/.bashrc
 source ~/.bashrc
-cd
 runuser -l root '/usr/sbin/usermod -aG $USER sudo' # sudoer
 runuser -l root '/usr/sbin/usermod -aG $USER vboxsf' # don't require su privileges to access VBox mounted disks
