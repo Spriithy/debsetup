d69f74a3633ea7b58ab85493a6fe64b62fa14aa0
Théophile Dano <theo.dano@gmail.com>
2019-10-23 10:26:15 +0200

    Create setup0.sh

---
+++ setup0.sh
@@ -0,0 +1,7 @@
+# Add user to sudoers
+
+echo "export PATH=$PATH:/usr/sbin/" >> ~/.bashrc
+source ~/.bashrc
+cd
+runuser -l root '/usr/sbin/usermod -aG $USER sudo' # sudoer
+runuser -l root '/usr/sbin/usermod -aG $USER vboxsf' # don't require su privileges to access VBox mounted disks
