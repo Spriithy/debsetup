6bd732059f7fe52fbc8f956052fcfa9dc3dfa147
Théophile Dano <theo.dano@gmail.com>
2019-10-23 11:24:33 +0200

    Update setup_sudo.sh

--- setup_sudo.sh
+++ setup_sudo.sh
@@ -2,3 +2,17 @@
 
 echo "export PATH=\$PATH:/usr/sbin/" >> ~/.bashrc
 su -c '/usr/sbin/usermod -aG sudo $USER'
+
+echo "Changes require the user to logout and login again before being effective."
+read -p "Do you want to logout now (y/N)? " answer
+case ${answer:0:1} in
+    y|Y )
+        if [[ $USER != "root" ]]; then
+            pkill -KILL -u $USER
+        fi
+        ;;
+    * )
+        echo "error: cannot logout root user."
+        echo "Please logout manually."
+        ;;
+esac
